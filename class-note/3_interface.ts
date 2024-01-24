ae63989a0d47476ea5b0c9cd4b69a2096e2ef9fa
kxxhan <ofliberty2@gmail.com>
2024-01-24 12:40:42 +0900

    Jan 24, 2023

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -1,67 +1,67 @@
 interface User {
-    age: number;
-    name: string;
+  age: number;
+  name: string;
 }
 
 // 1. 변수에 인터페이스 활용
 var seho: User = {
-    age: 33,
-    name: 'seho'
-}
+  age: 33,
+  name: "seho",
+};
 
 // 2. 함수에 인터페이스 활용
 function getUser(user: User) {
-    console.log(user)
+  console.log(user);
 }
 const user1 = {
-    age: 3,
-    name: "jhin"
-}
+  age: 3,
+  name: "jhin",
+};
 getUser(user1);
 
 // 3. 함수의 스펙(구조)를 정의하는 인터페이스
 interface SumFunction {
-    (a: number, b: number): number;
+  (a: number, b: number): number;
 }
 
 let sum: SumFunction;
-sum = function(a: number, b: number): number {
-    return a + b;
-}
+sum = function (a: number, b: number): number {
+  return a + b;
+};
 
 // 4. 인덱싱
 interface StringArray {
-    [index: number]: string;
+  [index: number]: string;
 }
 
-let arr: StringArray = ['a', 'b', 'c'];
+let arr: StringArray = ["a", "b", "c"];
 // arr[0] = 10 Error Occur!!!
 
 // 5. 인터페이스 딕셔너리 패턴
 interface StringRegexDictionary {
-    [key: string]: RegExp
+  [key: string]: RegExp;
 }
 
 let obj: StringRegexDictionary = {
-    // sth: /abc/,
-    cssFile: /\.css$/,
-    jsFile:/\.js$/,
-}
+  // sth: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+};
 
 // 5. 인터페이스 확장
 interface Person {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 
 interface Developer extends Person {
-    lang: string;
+  lang: string;
 }
 
 let captain: Developer = {
-    name: 'cpt',
-    age: 100,
-    lang: 'ts'
-}
+  name: "cpt",
+  age: 100,
+  lang: "ts",
+};
 
-export {}
+export {};
--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -2,28 +2,28 @@
 // 하나 이상의 타입을 쓸 수 있게함
 let v0110: string | number | boolean;
 function logMessage(value: string | number) {
-    if (typeof value === 'number') {
-        // number Method를 바로 사용할 수 있다.
-        value.toLocaleString();
-    }
-    if (typeof value === 'string') {
-        value.toString();
-    }
+  if (typeof value === "number") {
+    // number Method를 바로 사용할 수 있다.
+    value.toLocaleString();
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
 }
 logMessage("hi");
 logMessage(100);
 
 interface Developer {
-    name: string;
-    skill: string;
+  name: string;
+  skill: string;
 }
 interface Person {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 function askSomeone(someone: Developer | Person) {
-    // skill, age는 자동완성되지 않는다.
-    someone.name;
+  // skill, age는 자동완성되지 않는다.
+  someone.name;
 }
 
-export {}
+export {};
